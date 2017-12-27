bb60e15231e37ed622d50379f36541819a8a7537
yichuan <1012027293@qq.com>
2017-12-27 11:05:56 +0800

    add  添加运单controller

---
+++ bill-client/src/main/java/cn/sisyphe/coffee/bill/controller/WayBillController.java
@@ -0,0 +1,33 @@
+package cn.sisyphe.coffee.bill.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * 运单
+ */
+@Controller
+public class WayBillController {
+
+    /**
+     * 创建订单
+     */
+    public void createWayBill() {
+    }
+
+    /**
+     * 修改订单
+     */
+    public void updateWayBill() {
+    }
+
+
+    /**
+     * 删除订单
+     */
+    public void deleteWayBill() {
+
+    }
+
+
+
+}
