a38076fc59ffc3460ef48a71c0ba720f77ac4027
yichuan <1012027293@qq.com>
2017-12-28 09:12:42 +0800

    add  运单类型

--- bill-core/src/main/java/cn/sisyphe/coffee/bill/domain/base/BillServiceFactory.java
+++ bill-core/src/main/java/cn/sisyphe/coffee/bill/domain/base/BillServiceFactory.java
@@ -43,6 +43,10 @@ public class BillServiceFactory {
             case RESTOCK:
                 billService = new RestockBillService(bill);
                 break;
+            case TRANSMIT:
+                //运货单
+                billService = new RestockBillService(bill);
+                break;
             default:
                 break;
         }
