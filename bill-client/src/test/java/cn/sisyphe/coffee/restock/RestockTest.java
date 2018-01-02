7bc26faef93131e4fe2858742129dd1480cf2645
xieweiguang <weiguang.xie@niit.com.cn>
2018-01-02 10:02:26 +0800

    just test something in test/restock

---
+++ bill-client/src/test/java/cn/sisyphe/coffee/restock/RestockTest.java
@@ -0,0 +1,13 @@
+package cn.sisyphe.coffee.restock;
+/**
+ *@date: 2018/1/2
+ *@description: for Restock Test
+ *@author：xieweiguang
+ */
+public class RestockTest {
+
+    public void Juice(){
+
+
+    }
+}
