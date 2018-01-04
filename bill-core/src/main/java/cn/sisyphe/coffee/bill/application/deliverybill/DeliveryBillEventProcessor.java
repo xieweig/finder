4f6c286a6aa96d668403248401f23dbd51210eed
yichuan <1012027293@qq.com>
2018-01-04 11:01:37 +0800

    new  配送单管理类编写

---
+++ bill-core/src/main/java/cn/sisyphe/coffee/bill/application/deliverybill/DeliveryBillEventProcessor.java
@@ -0,0 +1,10 @@
+package cn.sisyphe.coffee.bill.application.deliverybill;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by Administrator on 2018/1/4.
+ */
+@Service
+public class DeliveryBillEventProcessor {
+}
---
+++ bill-core/src/main/java/cn/sisyphe/coffee/bill/application/deliverybill/DeliveryBillManager.java
@@ -0,0 +1,41 @@
+package cn.sisyphe.coffee.bill.application.deliverybill;
+
+import cn.sisyphe.coffee.bill.application.base.AbstractBillManager;
+import cn.sisyphe.coffee.bill.domain.delivery.DeliveryBill;
+import cn.sisyphe.coffee.bill.domain.plan.PlanBill;
+import cn.sisyphe.coffee.bill.infrastructure.base.BillRepository;
+import cn.sisyphe.coffee.bill.infrastructure.delivery.DeliveryBillRepository;
+import cn.sisyphe.coffee.bill.viewmodel.deliverybill.DeliveryBillDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by Administrator on 2018/1/4.
+ */
+@Service
+public class DeliveryBillManager extends AbstractBillManager<PlanBill> {
+
+    @Autowired
+    private DeliveryBillRepository deliveryBillRepository;
+
+
+    @Autowired
+    public DeliveryBillManager(BillRepository<PlanBill> billRepository, ApplicationEventPublisher applicationEventPublisher) {
+        super(billRepository, applicationEventPublisher);
+    }
+
+
+    /**
+     * 根据单号查看单据
+     *
+     * @param billCode
+     * @return
+     */
+    public DeliveryBillDTO findOneByBillCode(String billCode) {
+
+        DeliveryBillDTO deliveryBillDTO = new DeliveryBillDTO();
+        DeliveryBill deliveryBill = deliveryBillRepository.findOneByBillCode(billCode);
+        return deliveryBillDTO.convertBillToDTO(deliveryBill);
+    }
+}
