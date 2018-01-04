3fe3023359add1d5fa8d8411e2b4f156c10480cc
bifenglin <515186469@qq.com>
2018-01-04 13:49:34 +0800

    修改restockBill实体类

--- bill-core/src/main/java/cn/sisyphe/coffee/bill/domain/restock/RestockBillDetail.java
+++ bill-core/src/main/java/cn/sisyphe/coffee/bill/domain/restock/RestockBillDetail.java
@@ -1,12 +1,15 @@
 package cn.sisyphe.coffee.bill.domain.restock;
 
 import cn.sisyphe.coffee.bill.domain.base.model.BillDetail;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import java.math.BigDecimal;
+import java.util.Date;
 import java.util.Objects;
 
 /**
@@ -19,73 +22,80 @@ import java.util.Objects;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonIgnoreProperties(value = {"hibernateLazyInitializer", "handler", "fieldHandler"})
 public class RestockBillDetail extends BillDetail {
-
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        BillDetail that = (BillDetail) o;
-        return Objects.equals(getBillDetailId(), that.getBillDetailId());
-    }
-
-    @Override
-    public int hashCode() {
-
-        return Objects.hash(getBillDetailId());
-    }
-
-    public Integer getTotalPrice() {
-        return totalPrice;
-    }
-
-    public void setTotalPrice(Integer totalPrice) {
-        this.totalPrice = totalPrice;
-    }
-
-    public String getDetailsRemarks() {
-        return detailsRemarks;
-    }
-
-    public void setDetailsRemarks(String detailsRemarks) {
-        this.detailsRemarks = detailsRemarks;
-    }
+    /**
+     * 生产日期
+     */
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    private Date dateInProduced;
 
     /**
-     *
-     *notes :
-     *  配送总价
+     * 单位进价
      */
-    private Integer totalPrice;
+    private BigDecimal unitPrice;
+
     /**
-     *
-     *notes :
-     *  货物数量
+     * 发货数量
      */
-    @Column
-    private Integer actualNumber;
-    @Column
-    private Integer expectedNumber;
-    @Column(length = 500)
-    private String detailsRemarks;
+    private int shippedNumber;
 
-    public Integer getActualNumber() {
-        return actualNumber;
+    /**
+     * 数量差值
+     */
+    private int differenceNumber;
+
+    /**
+     * 总价差值
+     */
+    private BigDecimal differencePrice;
+
+    public Date getDateInProduced() {
+        return dateInProduced;
     }
 
-    public void setActualNumber(Integer actualNumber) {
-        this.actualNumber = actualNumber;
+    public void setDateInProduced(Date dateInProduced) {
+        this.dateInProduced = dateInProduced;
     }
 
-    public Integer getExpectedNumber() {
-        return expectedNumber;
+    public BigDecimal getUnitPrice() {
+        return unitPrice;
     }
 
-    public void setExpectedNumber(Integer expectedNumber) {
-        this.expectedNumber = expectedNumber;
+    public void setUnitPrice(BigDecimal unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+
+    public int getShippedNumber() {
+        return shippedNumber;
+    }
+
+    public void setShippedNumber(int shippedNumber) {
+        this.shippedNumber = shippedNumber;
+    }
+
+    public int getDifferenceNumber() {
+        return differenceNumber;
+    }
+
+    public void setDifferenceNumber(int differenceNumber) {
+        this.differenceNumber = differenceNumber;
+    }
+
+    public BigDecimal getDifferencePrice() {
+        return differencePrice;
+    }
+
+    public void setDifferencePrice(BigDecimal differencePrice) {
+        this.differencePrice = differencePrice;
+    }
+
+    @Override
+    public String toString() {
+        return "RestockBillDetail{" +
+                "dateInProduced=" + dateInProduced +
+                ", unitPrice=" + unitPrice +
+                ", shippedNumber=" + shippedNumber +
+                ", differenceNumber=" + differenceNumber +
+                ", differencePrice=" + differencePrice +
+                '}';
     }
 }
