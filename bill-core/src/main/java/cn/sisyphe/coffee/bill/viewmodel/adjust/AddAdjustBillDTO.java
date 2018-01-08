f50bca5ba0d30388bb786e486c9acc7c15fc343e
xj <728793963@qq.com>
2018-01-08 16:05:10 +0800

    [xj] 修改调剂单据DTO

--- bill-core/src/main/java/cn/sisyphe/coffee/bill/viewmodel/adjust/AddAdjustBillDTO.java
+++ bill-core/src/main/java/cn/sisyphe/coffee/bill/viewmodel/adjust/AddAdjustBillDTO.java
@@ -94,6 +94,162 @@ public class AddAdjustBillDTO {
      */
     private List<AdjustBillDetailDTO> details;
 
+    public String getBillCode() {
+        return billCode;
+    }
 
+    public void setBillCode(String billCode) {
+        this.billCode = billCode;
+    }
 
+    public Date getOutWareHouseTime() {
+        return outWareHouseTime;
+    }
+
+    public void setOutWareHouseTime(Date outWareHouseTime) {
+        this.outWareHouseTime = outWareHouseTime;
+    }
+
+    public String getOperatorCode() {
+        return operatorCode;
+    }
+
+    public void setOperatorCode(String operatorCode) {
+        this.operatorCode = operatorCode;
+    }
+
+    public String getAuditPersonCode() {
+        return auditPersonCode;
+    }
+
+    public void setAuditPersonCode(String auditPersonCode) {
+        this.auditPersonCode = auditPersonCode;
+    }
+
+    public String getOutStationCode() {
+        return outStationCode;
+    }
+
+    public void setOutStationCode(String outStationCode) {
+        this.outStationCode = outStationCode;
+    }
+
+    public Storage getOutStorage() {
+        return outStorage;
+    }
+
+    public void setOutStorage(Storage outStorage) {
+        this.outStorage = outStorage;
+    }
+
+    public String getInStationCode() {
+        return inStationCode;
+    }
+
+    public void setInStationCode(String inStationCode) {
+        this.inStationCode = inStationCode;
+    }
+
+    public String getBillType() {
+        return billType;
+    }
+
+    public void setBillType(String billType) {
+        this.billType = billType;
+    }
+
+    public String getOutStatusCode() {
+        return outStatusCode;
+    }
+
+    public void setOutStatusCode(String outStatusCode) {
+        this.outStatusCode = outStatusCode;
+    }
+
+    public String getSubmitState() {
+        return submitState;
+    }
+
+    public void setSubmitState(String submitState) {
+        this.submitState = submitState;
+    }
+
+    public String getAuditState() {
+        return auditState;
+    }
+
+    public void setAuditState(String auditState) {
+        this.auditState = auditState;
+    }
+
+    public Integer getAdjustNumber() {
+        return adjustNumber;
+    }
+
+    public void setAdjustNumber(Integer adjustNumber) {
+        this.adjustNumber = adjustNumber;
+    }
+
+    public Integer getVarietyNumber() {
+        return varietyNumber;
+    }
+
+    public void setVarietyNumber(Integer varietyNumber) {
+        this.varietyNumber = varietyNumber;
+    }
+
+    public String getPlanMemo() {
+        return planMemo;
+    }
+
+    public void setPlanMemo(String planMemo) {
+        this.planMemo = planMemo;
+    }
+
+    public String getOutStorageMemo() {
+        return outStorageMemo;
+    }
+
+    public void setOutStorageMemo(String outStorageMemo) {
+        this.outStorageMemo = outStorageMemo;
+    }
+
+    public String getAuditMemo() {
+        return auditMemo;
+    }
+
+    public void setAuditMemo(String auditMemo) {
+        this.auditMemo = auditMemo;
+    }
+
+    public List<AdjustBillDetailDTO> getDetails() {
+        return details;
+    }
+
+    public void setDetails(List<AdjustBillDetailDTO> details) {
+        this.details = details;
+    }
+
+    @Override
+    public String toString() {
+        return "AddAdjustBillDTO{" +
+                "billCode='" + billCode + '\'' +
+                ", outWareHouseTime=" + outWareHouseTime +
+                ", operatorCode='" + operatorCode + '\'' +
+                ", auditPersonCode='" + auditPersonCode + '\'' +
+                ", outStationCode='" + outStationCode + '\'' +
+                ", outStorage=" + outStorage +
+                ", inStationCode='" + inStationCode + '\'' +
+                ", billType='" + billType + '\'' +
+                ", outStatusCode='" + outStatusCode + '\'' +
+                ", submitState='" + submitState + '\'' +
+                ", auditState='" + auditState + '\'' +
+                ", adjustNumber=" + adjustNumber +
+                ", varietyNumber=" + varietyNumber +
+                ", planMemo='" + planMemo + '\'' +
+                ", outStorageMemo='" + outStorageMemo + '\'' +
+                ", auditMemo='" + auditMemo + '\'' +
+                ", details=" + details +
+                '}';
+    }
 }
--- bill-core/src/main/java/cn/sisyphe/coffee/bill/viewmodel/adjust/AdjustBillDetailDTO.java
+++ bill-core/src/main/java/cn/sisyphe/coffee/bill/viewmodel/adjust/AdjustBillDetailDTO.java
@@ -2,8 +2,11 @@ package cn.sisyphe.coffee.bill.viewmodel.adjust;
 
 /**
  * Created by XiongJing on 2018/1/8.
- * remark：
- * version:
+ * remark：调剂单据明细DTO
+ * version: 1.0
+ *
+ * @author XiongJing
  */
 public class AdjustBillDetailDTO {
+
 }
