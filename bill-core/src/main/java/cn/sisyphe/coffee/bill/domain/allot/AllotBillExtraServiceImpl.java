2aab42d5287295430407809e39784edacbfec91f
bifenglin <515186469@qq.com>
2018-01-12 14:28:47 +0800

    配送模块包结构

---
+++ bill-core/src/main/java/cn/sisyphe/coffee/bill/domain/allot/AllotBillExtraServiceImpl.java
@@ -0,0 +1,4 @@
+package cn.sisyphe.coffee.bill.domain.allot;
+
+public class AllotBillExtraServiceImpl implements AllotBillExtraService {
+}
---
+++ bill-core/src/main/java/cn/sisyphe/coffee/bill/domain/allot/AllotBillService.java
@@ -0,0 +1,30 @@
+package cn.sisyphe.coffee.bill.domain.allot;
+
+import cn.sisyphe.coffee.bill.domain.base.AbstractBillService;
+import cn.sisyphe.coffee.bill.domain.base.model.Bill;
+
+/**
+ * @author heyong
+ */
+public class AllotBillService extends AbstractBillService {
+
+    public AllotBillService(Bill bill) {
+        super(bill);
+    }
+
+    /**
+     * 某个单据特殊前置动作处理
+     */
+    @Override
+    public void beforeDispose() {
+        // TODO: 2017/12/27 在操作之前的一些事情
+    }
+
+    /**
+     * 某个单据特殊后置动作处理
+     */
+    @Override
+    public void afterDispose() {
+        // TODO: 2017/12/27 在操作之后的一些事情
+    }
+}
