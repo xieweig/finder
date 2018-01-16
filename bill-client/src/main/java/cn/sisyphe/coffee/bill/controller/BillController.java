74a51951bce56e0ea5621b5b0162f1f5d6c4d07f
mayupeng <mayupeng@sisyphe.cn>
2018-01-16 17:28:32 +0800

    BillController整理

--- bill-client/src/main/java/cn/sisyphe/coffee/bill/controller/BillController.java
+++ bill-client/src/main/java/cn/sisyphe/coffee/bill/controller/BillController.java
@@ -4,6 +4,8 @@ import cn.sisyphe.coffee.bill.application.adjust.AdjustBillManager;
 import cn.sisyphe.coffee.bill.application.base.AbstractBillManager;
 import cn.sisyphe.coffee.bill.application.base.BillManagerFactory;
 import cn.sisyphe.coffee.bill.application.plan.PlanBillManager;
+import cn.sisyphe.coffee.bill.domain.adjust.model.AdjustBill;
+import cn.sisyphe.coffee.bill.domain.adjust.model.AdjustBillDetail;
 import cn.sisyphe.coffee.bill.domain.base.model.Bill;
 import cn.sisyphe.coffee.bill.domain.base.model.enums.BillPurposeEnum;
 import cn.sisyphe.coffee.bill.domain.base.model.enums.BillTypeEnum;
@@ -49,23 +51,17 @@ public abstract class BillController<T extends Bill> {
     }
 
     /**
-     * 计划单据多条件分页查询
+     * 多条件分页查询计划单据
      *
      * @return
      */
-    @ApiOperation(value = "子计划多条件查询")
+    @ApiOperation(value = "多条件分页查询计划单据")
     @RequestMapping(path = "/findPlanByConditions", method = RequestMethod.POST)
     public ResponseResult findPlanByConditions(@RequestBody ConditionQueryPlanBill conditionQueryPlanBill) {
         ResponseResult responseResult = new ResponseResult();
         try {
             Page<ChildPlanBillDTO> planBillDTOS = planBillManager.findChildPlanBillByCondition(conditionQueryPlanBill,
                     BillTypeEnum.ADJUST, BillPurposeEnum.OUT_STORAGE);
-
-            List<String> restockCodeList = new ArrayList<>();
-            for (ChildPlanBillDTO childPlanBillDTO : planBillDTOS) {
-                restockCodeList.add(childPlanBillDTO.getBillCode());
-            }
-            responseResult.put("content", planBillDTOS);
         } catch (DataException e) {
             responseResult.putException(e);
         }
@@ -73,12 +69,12 @@ public abstract class BillController<T extends Bill> {
     }
 
     /**
-     * 子计划直接查看已保存的拣货单
+     * 根据源单号查询单据详细信息
      *
      * @param sourceCode
      * @return
      */
-    @ApiOperation(value = "子计划直接查看已保存的拣货单")
+    @ApiOperation(value = "根据源单号查询单据详细信息")
     @RequestMapping(path = "/findBySourceCode", method = RequestMethod.GET)
     public ResponseResult findBySourceCode(@RequestParam("sourceCode") String sourceCode) {
         ResponseResult responseResult = new ResponseResult();
@@ -91,12 +87,12 @@ public abstract class BillController<T extends Bill> {
     }
 
     /**
-     * 查询子计划单个查询
+     * 根据单据号查询计划单据详细信息
      *
-     * @param billCode
+     * @param billCode 单据号
      * @return
      */
-    @ApiOperation(value = "查询子计划单个查询")
+    @ApiOperation(value = "根据单据号查询计划单据详细信息")
     @RequestMapping(path = "/findPlanByBillCode", method = RequestMethod.GET)
     public ResponseResult findPlanByBillCode(@RequestParam("billCode") String billCode) {
         ResponseResult responseResult = new ResponseResult();
@@ -109,44 +105,50 @@ public abstract class BillController<T extends Bill> {
     }
 
     /**
-     * 多条件查询调剂出库单据
+     * 多条件分页查询出库单据
      *
-     * @param conditionQueryAdjustBill 查询条件
+     * @param conditionQueryAdjustBill 查询条件DTO
      * @return
      */
-    @ApiOperation(value = "多条件查询调剂出库单据")
+    @ApiOperation(value = "多条件分页查询出库单据")
     @RequestMapping(path = "/findOutStorageByConditions", method = RequestMethod.POST)
     @ScopeAuth(scopes = {"#conditionQueryAdjustBill.outStationCodeArray", "#conditionQueryAdjustBill.inStationCodeArray"}, token = "userCode")
     public ResponseResult findOutStorageByConditions(@RequestBody ConditionQueryAdjustBill conditionQueryAdjustBill) {
         ResponseResult responseResult = new ResponseResult();
-//        Page<AdjustBillDTO> dtoPage = BillManagerFactory.getManager(billTypeEnum).(conditionQueryAdjustBill);
-//        responseResult.put("content", dtoPage);
+        try {
+//            responseResult.put("adjustBill", adjustBillManager.findByBillCode(billCode));
+        } catch (DataException data) {
+            responseResult.putException(data);
+        }
         return responseResult;
     }
 
     /**
-     * 多条件查询调剂入库单据
+     * 多条件分页查询入库单据
      *
-     * @param conditionQueryAdjustBill 查询条件
+     * @param conditionQueryAdjustBill 查询条件DTO
      * @return
      */
-    @ApiOperation(value = "多条件查询调剂入库单据")
+    @ApiOperation(value = "多条件分页查询入库单据")
     @RequestMapping(path = "/findInStorageByConditions", method = RequestMethod.POST)
     @ScopeAuth(scopes = {"#conditionQueryPlanBill.outStationCodeArray", "#conditionQueryPlanBill.inStationCodeArray"}, token = "userCode")
     public ResponseResult findInStorageByConditions(@RequestBody ConditionQueryAdjustBill conditionQueryAdjustBill) {
         ResponseResult responseResult = new ResponseResult();
-//        Page<AdjustBillDTO> dtoPage = adjustBillManager.findByConditionsToIn(conditionQueryAdjustBill);
-//        responseResult.put("content", dtoPage);
+        try {
+//            responseResult.put("adjustBill", adjustBillManager.findByBillCode(billCode));
+        } catch (DataException data) {
+            responseResult.putException(data);
+        }
         return responseResult;
     }
 
     /**
-     * 根据调剂单号查询详细信息
+     * 根据单据号查询单据详细信息
      *
-     * @param billCode 调剂单号
+     * @param billCode 单据号
      * @return
      */
-    @ApiOperation(value = "根据调剂单号查询详细信息")
+    @ApiOperation(value = "根据单据号查询详细信息")
     @RequestMapping(path = "/findByBillCode", method = RequestMethod.GET)
     public ResponseResult findByBillCode(@RequestParam(value = "billCode") String billCode) {
         ResponseResult responseResult = new ResponseResult();
@@ -159,17 +161,17 @@ public abstract class BillController<T extends Bill> {
     }
 
     /**
-     * 审核按钮--打开操作
+     * 根据单据号打开单据
      *
-     * @param billCode 调剂单号
+     * @param billCode 单据号
      * @return
      */
-    @ApiOperation(value = "根据调剂单号查询详细信息")
+    @ApiOperation(value = "根据单据号打开单据")
     @RequestMapping(path = "/open", method = RequestMethod.GET)
     public ResponseResult open(HttpServletRequest request, @RequestParam(value = "billCode") String billCode) {
         ResponseResult responseResult = new ResponseResult();
-        LoginInfo loginInfo = LoginInfo.getLoginInfo(request);
         try {
+            LoginInfo loginInfo = LoginInfo.getLoginInfo(request);
 //            responseResult.put("adjustBill", adjustBillManager.openBill(billCode, loginInfo.getOperatorCode()));
         } catch (DataException data) {
             responseResult.putException(data);
@@ -178,19 +180,19 @@ public abstract class BillController<T extends Bill> {
     }
 
     /**
-     * 保存调剂单据信息
+     * 保存单据信息
      *
-     * @param addAdjustBillDTO
+     * @param addAdjustBillDTO 单据DTO
      * @return
      */
-    @ApiOperation(value = "保存调剂单据信息  ")
+    @ApiOperation(value = "保存单据信息")
     @RequestMapping(path = "/save", method = RequestMethod.POST)
     public ResponseResult save(HttpServletRequest request, @RequestBody AddAdjustBillDTO addAdjustBillDTO) {
-
         ResponseResult responseResult = new ResponseResult();
         try {
-//            LoginInfo loginInfo = LoginInfo.getLoginInfo(request);
-//            addAdjustBillDTO.setOperatorCode(loginInfo.getOperatorCode());
+            LoginInfo loginInfo = LoginInfo.getLoginInfo(request);
+            addAdjustBillDTO.setOperatorCode(loginInfo.getOperatorCode());
+            //测试使用
             addAdjustBillDTO.setOperatorCode("test0001");
 //            responseResult.put("billCode", adjustBillManager.create(addAdjustBillDTO));
         } catch (DataException data) {
@@ -200,20 +202,20 @@ public abstract class BillController<T extends Bill> {
     }
 
     /**
-     * 提交调剂单据信息
+     * 提交单据信息
      *
-     * @param addAdjustBillDTO
+     * @param addAdjustBillDTO 单据DTO
      * @return
      */
-    @ApiOperation(value = "提交调剂单据信息")
+    @ApiOperation(value = "提交单据信息")
     @RequestMapping(path = "/submit", method = RequestMethod.POST)
     public ResponseResult submit(HttpServletRequest request, @RequestBody AddAdjustBillDTO addAdjustBillDTO) {
         ResponseResult responseResult = new ResponseResult();
         try {
-//            LoginInfo loginInfo = LoginInfo.getLoginInfo(request);
-//            addAdjustBillDTO.setOperatorCode(loginInfo.getOperatorCode());
-            addAdjustBillDTO.setOperatorCode("test0001");
-//            responseResult.put("billCode", adjustBillManager.submit(addAdjustBillDTO));
+            LoginInfo loginInfo = LoginInfo.getLoginInfo(request);
+            addAdjustBillDTO.setOperatorCode(loginInfo.getOperatorCode());
+            //addAdjustBillDTO.setOperatorCode("test0001");
+            //responseResult.put("billCode", abstractBillManager.submit(addAdjustBillDTO));
         } catch (DataException data) {
             responseResult.putException(data);
         }
@@ -223,30 +225,38 @@ public abstract class BillController<T extends Bill> {
     /**
      * 审核不通过
      *
-     * @param adjustBillCode 调剂单编码
+     * @param billCode 单据号
      * @return
      */
     @ApiOperation(value = "审核不通过")
     @RequestMapping(path = "/auditFailure", method = RequestMethod.POST)
-    public ResponseResult auditFailure(HttpServletRequest request, @RequestParam(value = "adjustBillCode") String adjustBillCode) {
-        LoginInfo loginInfo = LoginInfo.getLoginInfo(request);
+    public ResponseResult auditFailure(HttpServletRequest request, @RequestParam(value = "billCode") String billCode) {
         ResponseResult responseResult = new ResponseResult();
-//        adjustBillManager.audit(adjustBillCode, "auditPerson001", false);
+        try {
+            LoginInfo loginInfo = LoginInfo.getLoginInfo(request);
+            //abstractBillManager.audit(billCode,false);
+        } catch (DataException data) {
+            responseResult.putException(data);
+        }
         return responseResult;
     }
 
     /**
      * 审核通过
      *
-     * @param adjustBillCode 调剂单编码
+     * @param billCode 单据号
      * @return
      */
     @ApiOperation(value = "审核通过")
     @RequestMapping(path = "/auditSuccess", method = RequestMethod.POST)
-    public ResponseResult auditSuccess(HttpServletRequest request, @RequestParam(value = "adjustBillCode") String adjustBillCode) {
-        LoginInfo loginInfo = LoginInfo.getLoginInfo(request);
+    public ResponseResult auditSuccess(HttpServletRequest request, @RequestParam(value = "billCode") String billCode) {
         ResponseResult responseResult = new ResponseResult();
-//        adjustBillManager.audit(adjustBillCode, "auditPerson001", true);
+        try {
+            LoginInfo loginInfo = LoginInfo.getLoginInfo(request);
+            //abstractBillManager.audit(billCode,true);
+        } catch (DataException data) {
+            responseResult.putException(data);
+        }
         return responseResult;
     }
 
