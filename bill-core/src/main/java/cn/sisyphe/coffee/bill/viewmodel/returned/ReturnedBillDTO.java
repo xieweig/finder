826c67cdcbd77763bebd3681f98476c404a20675
bifenglin <515186469@qq.com>
2018-01-17 13:46:25 +0800

    总价字段修改到退货单

--- bill-core/src/main/java/cn/sisyphe/coffee/bill/viewmodel/returned/ReturnedBillDTO.java
+++ bill-core/src/main/java/cn/sisyphe/coffee/bill/viewmodel/returned/ReturnedBillDTO.java
@@ -3,9 +3,24 @@ package cn.sisyphe.coffee.bill.viewmodel.returned;
 import cn.sisyphe.coffee.bill.viewmodel.base.BillDTO;
 import cn.sisyphe.coffee.bill.viewmodel.restock.RestockBillDetailDTO;
 
+import java.math.BigDecimal;
+
 /**
  * Created by heyong on 2018/1/17 11:41
  * Description:
  */
 public class ReturnedBillDTO extends BillDTO<ReturnedBillDetailDTO> {
+
+    /**
+     * 总价
+     */
+    private BigDecimal totalPrice;
+
+    public BigDecimal getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(BigDecimal totalPrice) {
+        this.totalPrice = totalPrice;
+    }
 }
