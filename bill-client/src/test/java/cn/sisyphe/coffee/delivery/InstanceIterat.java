acbb9ce2e023f4929612b6774cfde5ebe9f7dc9b
xieweiguang <weiguang.xie@niit.com.cn>
2018-01-18 16:44:28 +0800

    delivery 数据库生成数据代码

--- bill-client/src/test/java/cn/sisyphe/coffee/delivery/InstanceIterat.java
+++ bill-client/src/test/java/cn/sisyphe/coffee/delivery/InstanceIterat.java
@@ -1,11 +1,12 @@
-
-package cn.sisyphe.coffee.returned;
+package cn.sisyphe.coffee.delivery;
 
 import cn.sisyphe.coffee.bill.domain.base.model.enums.BasicEnum;
 import cn.sisyphe.coffee.bill.domain.base.model.enums.BillPurposeEnum;
 import cn.sisyphe.coffee.bill.domain.base.model.enums.BillTypeEnum;
 import cn.sisyphe.coffee.bill.domain.base.model.goods.Cargo;
 import cn.sisyphe.coffee.bill.domain.base.model.goods.RawMaterial;
+import cn.sisyphe.coffee.bill.viewmodel.delivery.DeliveryBillDTO;
+import cn.sisyphe.coffee.bill.viewmodel.delivery.DeliveryBillDetailDTO;
 import cn.sisyphe.coffee.bill.viewmodel.returned.ReturnedBillDTO;
 import cn.sisyphe.coffee.bill.viewmodel.returned.ReturnedBillDetailDTO;
 import cn.sisyphe.coffee.restock.InstanceIter;
@@ -16,27 +17,32 @@ import org.slf4j.LoggerFactory;
 import java.util.HashSet;
 import java.util.Set;
 
-public class InstanceIterator extends InstanceIter{
-
-    public static final String[] SOURCE_CODE_FOR_RETURNED={"1516262901500","1516262901605","1516262901623"};
+/**
+ * @Author: xie_wei_guang
+ * @Date: 2018/1/18
+ * @Description:
+ */
+public class InstanceIterat extends InstanceIter{
+    //请先运行for plan 插入plan数据，之后替换下面，可能需要注释掉send event
+    public static final String[] SOURCE_CODE_FOR_DELIVERY={"1516264416977","1516264417008","1516264416970"};
     Logger logger = LoggerFactory.getLogger(this.getClass());
 
 
-    public ReturnedBillDTO nextRandomAddReturnedBillDTO(Integer detailsNumber){
+    public DeliveryBillDTO nextRandomAddDeliveryBillDTO(Integer detailsNumber){
 
-        ReturnedBillDTO dto = new ReturnedBillDTO();
+        DeliveryBillDTO dto = new DeliveryBillDTO();
 
         //可能有误
 
 
-        dto.setBillType(BillTypeEnum.RETURNED);
+        dto.setBillType(BillTypeEnum.DELIVERY);
         dto.setBillPurpose(BillPurposeEnum.OUT_STORAGE);
         Integer sign = random.nextInt(10);
 
 
         if (sign>4){
-            dto.setSpecificBillType(BillTypeEnum.RETURNED);
-            dto.setSourceCode(SOURCE_CODE_FOR_RETURNED[random.nextInt(SOURCE_CODE_FOR_RETURNED.length)].trim());
+            dto.setSpecificBillType(BillTypeEnum. DELIVERY);
+            dto.setSourceCode(SOURCE_CODE_FOR_DELIVERY[random.nextInt(SOURCE_CODE_FOR_DELIVERY.length)].trim());
         }
 
         else{
@@ -49,9 +55,9 @@ public class InstanceIterator extends InstanceIter{
         dto.setBasicEnum(BasicEnum.values()[random.nextInt(BasicEnum.values().length)]);
 
 
-        Set<ReturnedBillDetailDTO> details = new HashSet();
+        Set<DeliveryBillDetailDTO> details = new HashSet();
         for (int i = 0; i < detailsNumber; i++) {
-            details.add(this.nextRandomReturnedBillDetailDTO());
+            details.add(this.nextRandomDeliveryBillDetailDTO());
         }
 
         dto.setBillDetails(details);
@@ -66,8 +72,8 @@ public class InstanceIterator extends InstanceIter{
 
         return dto;
     }
-    protected ReturnedBillDetailDTO nextRandomReturnedBillDetailDTO(){
-        ReturnedBillDetailDTO  DetailDTO = new ReturnedBillDetailDTO();
+    protected DeliveryBillDetailDTO nextRandomDeliveryBillDetailDTO(){
+        DeliveryBillDetailDTO  DetailDTO = new DeliveryBillDetailDTO();
 
         Integer amount = random.nextInt(100)+20;
         DetailDTO.setActualAmount(amount);
@@ -82,11 +88,6 @@ public class InstanceIterator extends InstanceIter{
         System.err.println(ToStringBuilder.reflectionToString(DetailDTO));
         return DetailDTO;
     }
-
-
-
-
-
 
 
 
---
+++ bill-client/src/test/java/cn/sisyphe/coffee/delivery/NewDeliveryTest.java
@@ -0,0 +1,33 @@
+package cn.sisyphe.coffee.delivery;
+
+import cn.sisyphe.coffee.bill.CoreApplication;
+import cn.sisyphe.coffee.bill.application.delivery.DeliveryBillManager;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
+
+/**
+ * @Author: xie_wei_guang
+ * @Date: 2018/1/18
+ * @Description:
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = {CoreApplication.class})
+public class NewDeliveryTest extends  InstanceIterat{
+
+    @Resource
+    private DeliveryBillManager deliveryBillManager;
+
+    @Test
+    public void saveDTO(){
+        for (int i = 0; i <6; i++) {
+            deliveryBillManager.saveBill(this.nextRandomAddDeliveryBillDTO(random.nextInt(3)+1));
+
+        }
+
+    }
+
+}
