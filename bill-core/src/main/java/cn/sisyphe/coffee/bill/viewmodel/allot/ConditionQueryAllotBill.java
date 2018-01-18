88a2198b51bac9a0d68d45b7be9b06d420ef330b
maoningchuan <minchina91@gmail.com>
2018-01-18 16:39:09 +0800

    增加调拨单查询

--- bill-core/src/main/java/cn/sisyphe/coffee/bill/viewmodel/allot/ConditionQueryAllotBill.java
+++ bill-core/src/main/java/cn/sisyphe/coffee/bill/viewmodel/allot/ConditionQueryAllotBill.java
@@ -1,16 +1,73 @@
 package cn.sisyphe.coffee.bill.viewmodel.allot;
 
-import cn.sisyphe.coffee.bill.domain.base.model.enums.BillTypeEnum;
-import cn.sisyphe.coffee.bill.domain.base.model.location.Storage;
 import cn.sisyphe.coffee.bill.viewmodel.base.ConditionQueryBill;
+import com.fasterxml.jackson.annotation.JsonFormat;
 
-import java.io.Serializable;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import java.util.Date;
 
 /**
  * @date: 2018/1/12
  * @description: 多条件查询退库调拨单查询
  * @author：bifenglin
  */
-public class ConditionQueryAllotBill extends ConditionQueryBill  {
+public class ConditionQueryAllotBill extends ConditionQueryBill {
 
+
+    /**
+     * 调拨开始时间
+     */
+    @Temporal(TemporalType.TIMESTAMP)
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    private Date allotStartTime;
+    /**
+     * 调拨结束时间
+     */
+    @Temporal(TemporalType.TIMESTAMP)
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    private Date allotEndTime;
+
+
+    /**
+     * 调出库位
+     */
+    private String outStorageCode;
+
+    /**
+     * 调入库位
+     */
+    private String inStorageCode;
+
+    public Date getAllotStartTime() {
+        return allotStartTime;
+    }
+
+    public void setAllotStartTime(Date allotStartTime) {
+        this.allotStartTime = allotStartTime;
+    }
+
+    public Date getAllotEndTime() {
+        return allotEndTime;
+    }
+
+    public void setAllotEndTime(Date allotEndTime) {
+        this.allotEndTime = allotEndTime;
+    }
+
+    public String getOutStorageCode() {
+        return outStorageCode;
+    }
+
+    public void setOutStorageCode(String outStorageCode) {
+        this.outStorageCode = outStorageCode;
+    }
+
+    public String getInStorageCode() {
+        return inStorageCode;
+    }
+
+    public void setInStorageCode(String inStorageCode) {
+        this.inStorageCode = inStorageCode;
+    }
 }
